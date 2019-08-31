d521a1488d98e37dcf107b69b8b42876d714356a
RebeccaMM <rebecca.moreiram@gmail.com>
2019-08-31 17:15:18 -0300

    Layout com Bootstrap

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-usuarios.herokuapp.com']
+ALLOWED_HOSTS = ['gestao-usuarios.herokuapp.com', 'localhost']
 
 # Application definition
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'clientes',
     'home'
 ]
@@ -125,3 +126,7 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'person_list'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    'statics'
+]
